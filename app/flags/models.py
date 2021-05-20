aa846a73ee18a979783e63df357779a4102333af
Alex Baikov <baikov52@gmail.com>
2021-05-20 11:19:59 +0300

    fix fields name in Seo model

--- app/flags/models.py
+++ app/flags/models.py
@@ -7,9 +7,9 @@ class Seo(models.Model):
     """Abstract class for SEO fields"""
 
     slug = models.SlugField(max_length=100, unique=True)
-    meta_title = models.CharField(verbose_name=_("SEO Title"), max_length=250, blank=True)
-    meta_description = models.TextField(max_length=400, verbose_name=_("SEO Description"), blank=True)
-    meta_h1 = models.CharField(verbose_name=_("SEO H1"), max_length=250, blank=True)
+    seo_title = models.CharField(verbose_name=_("SEO Title"), max_length=250, blank=True)
+    seo_description = models.TextField(max_length=400, verbose_name=_("SEO Description"), blank=True)
+    seo_h1 = models.CharField(verbose_name=_("SEO H1"), max_length=250, blank=True)
     is_published = models.BooleanField(verbose_name=_("Published"), default=False, null=False)
     is_index = models.BooleanField(verbose_name=_("index"), default=True, null=False)
     is_follow = models.BooleanField(verbose_name=_("follow"), default=True, null=False)
