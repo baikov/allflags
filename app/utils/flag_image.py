815d80105e0dd172176472a55bc116153953d105
Alex Baikov <baikov52@gmail.com>
2021-07-08 15:12:31 +0300

    added function for downloading flags images

--- app/utils/flag_image.py
+++ app/utils/flag_image.py
@@ -28,25 +28,38 @@ def get_historical_flag_img(url, from_year, to_year, country_iso2):
     return f"historical-flags/{iso2}/{file_name}"
 
 
-# def get_flag_img(iso2):
-#     sizes = [
-#         'w20', 'w40', 'w80', 'w160', 'w320', 'w640', 'w1280', 'w2560',
-#         'h20', 'h24', 'h40', 'h60', 'h80', 'h120', 'h240'
-#         ]
-#     bitmap = ['png', 'jpg', 'webp']
-#     vector = ['svg', 'ai', 'pdf', 'eps']
-#     cdn = 'https://flagcdn.com'
-#     save_to_path = f'{MEDIA_ROOT}/national-flags'
-#     iso2 = iso2.lower()
-#     for format in bitmap:
-#         for size in sizes:
-#             url = f'{cdn}/{size}/{iso2}.{format}'
-#             dest = f'{save_to_path}/{iso2}/{size}'
-#             file_name = f'{iso2}.{format}'
-#             download_img(url, dest, file_name)
+def get_flag_img(iso2):
+    sizes = [
+        "w20",
+        "w40",
+        "w80",
+        "w160",
+        "w320",
+        "w640",
+        "w1280",
+        "w2560",
+        "h20",
+        "h24",
+        "h40",
+        "h60",
+        "h80",
+        "h120",
+        "h240",
+    ]
+    bitmap = ["png", "jpg", "webp"]
+    vector = ["svg", "ai", "pdf", "eps"]
+    cdn = "https://flagcdn.com"
+    save_to_path = f"{MEDIA_ROOT}/national-flags"
+    iso2 = iso2.lower()
+    for format in bitmap:
+        for size in sizes:
+            url = f"{cdn}/{size}/{iso2}.{format}"
+            dest = f"{save_to_path}/{iso2}/{size}"
+            file_name = f"{iso2}.{format}"
+            download_img(url, dest, file_name)
 
-#     for format in vector:
-#         url = f'{cdn}/{iso2}.{format}'
-#         dest = f'{save_to_path}/{iso2}'
-#         file_name = f'{iso2}.{format}'
-#         download_img(url, dest, file_name)
+    for format in vector:
+        url = f"{cdn}/{iso2}.{format}"
+        dest = f"{save_to_path}/{iso2}"
+        file_name = f"{iso2}.{format}"
+        download_img(url, dest, file_name)
