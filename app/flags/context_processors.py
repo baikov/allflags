d9f9217373061580fade95f4799415c13779a6e2
Alex Baikov <baikov52@gmail.com>
2021-07-23 13:07:09 +0300

    change regions menu context

--- app/flags/context_processors.py
+++ app/flags/context_processors.py
@@ -2,7 +2,9 @@ from app.flags.models import Region
 
 
 def regions_menu(request):
-    # categories = Region.objects.filter(parent=None)
-    regions = Region.objects.all()
+    if request.user.is_superuser:
+        regions = Region.objects.filter(parent=None).order_by("ordering")
+    else:
+        regions = Region.objects.filter(parent=None, is_published=True).order_by("ordering")
 
     return {"regions_menu": regions}
