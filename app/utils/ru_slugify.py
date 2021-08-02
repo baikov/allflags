6871546742275025761ac835f7707263b88fffc0
Alex Baikov <baikov52@gmail.com>
2021-08-02 14:52:10 +0300

    added custom_slugify func for slugify cyrilic string

---
+++ app/utils/ru_slugify.py
@@ -0,0 +1,47 @@
+from django.utils.text import slugify
+
+
+def custom_slugify(string: str) -> str:
+    letters = {
+        "а": "a",
+        "б": "b",
+        "в": "v",
+        "г": "g",
+        "д": "d",
+        "е": "e",
+        "ё": "yo",
+        "ж": "zh",
+        "з": "z",
+        "и": "i",
+        "й": "y",
+        "к": "k",
+        "л": "l",
+        "м": "m",
+        "н": "n",
+        "о": "o",
+        "п": "p",
+        "р": "r",
+        "с": "s",
+        "т": "t",
+        "у": "u",
+        "ф": "f",
+        "х": "h",
+        "ц": "ts",
+        "ч": "ch",
+        "ш": "sh",
+        "щ": "shch",
+        "ъ": "",
+        "ы": "i",
+        "ь": "",
+        "э": "e",
+        "ю": "yu",
+        "я": "ya",
+    }
+
+    string = string.lower()
+
+    for char in set(string):
+        if char in letters.keys():
+            string = string.replace(char, letters[char])
+
+    return slugify(string)
