fc683bc7be427019aa55f9e6ce02357b807fc5b6
Alex Baikov <baikov52@gmail.com>
2021-08-09 20:27:07 +0300

    optimized download and convert funcs

--- app/utils/flag_image.py
+++ app/utils/flag_image.py
@@ -12,22 +12,33 @@ def delete_img(file):
     os.remove(file)
 
 
-def download_img(url, path, name):
-    file = f"{path}/{name}"
+def download_img(url: str, path: str, file_name: str) -> str:
+    *_, ext = url.split("/")[-1].split(".")
+    file = f"{path}/{file_name}.{ext}"
     Path(path).mkdir(parents=True, exist_ok=True)
-    r = requests.get(url)
-    # print(f'{url}: {r.status_code}')
-    with open(file, "wb") as f:
-        f.write(r.content)
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",  # noqa E501
+        "Accept-Language": "en-us",
+    }
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        with open(file, "wb") as f:
+            f.write(r.content)
+        return f"{file_name}.{ext}"
+    else:
+        return "status-code-not-200.png"
 
 
 def get_historical_flag_img(url, from_year, to_year, country_iso2):
     iso2 = country_iso2.lower()
     path = f"{MEDIA_ROOT}/historical-flags/{iso2}"
     file_name = f"{iso2}-{from_year}-{to_year}"
-    download_img(url, path, f"{file_name}.svg")
-    svg_convert(path, file_name)
-    return f"historical-flags/{iso2}/{file_name}"
+    saved_file = download_img(url, path, file_name)
+    # svg_convert(path, file_name)
+    # return f"historical-flags/{iso2}/{file_name}"
+    return f"historical-flags/{iso2}/{saved_file}"
 
 
 def get_flag_img(iso2):
@@ -70,29 +81,47 @@ def get_flag_img(iso2):
 
 def get_construction_img(url, iso2):
     iso2 = iso2.lower()
-    path = f"{MEDIA_ROOT}/construction"
+    path = os.path.join(MEDIA_ROOT, "construction")
     file_name = f"{iso2}-flag-construction"
-    download_img(url, path, f"{file_name}.svg")
-    svg_convert(path, file_name)
+
+    saved_file = download_img(url, path, file_name)
+    # svg_convert(path, file_name)
     # return f"construction/{file_name}"
-    return file_name
+    return f"construction/{saved_file}"
 
 
-def svg_convert(path, file_name):
-    # destination_path = f"{MEDIA_ROOT}/construction"
-    # file_name, _ = svg_file.split('.', maxsplit=1)
+def svg_convert(file_name: str) -> None:
 
-    try:
-        cairosvg.svg2png(url=f"{path}/{file_name}.svg", output_width=1000, write_to=f"{path}/{file_name}.png")
-    except Exception:
-        cairosvg.svg2png(
-            url=f"{path}/{file_name}.svg",
-            parent_width=800,
-            parent_height=600,
-            output_width=1000,
-            write_to=f"{path}/{file_name}.png",
-        )
+    path = "/".join(file_name.split("/")[:-1])
+    file_name, *_, ext = file_name.split("/")[-1].split(".")
+    ext = ext.lower()
+
+    if ext == "svg":
+        try:
+            cairosvg.svg2png(url=f"{path}/{file_name}.svg", output_width=1000, write_to=f"{path}/{file_name}.png")
+        except Exception:
+            cairosvg.svg2png(
+                url=f"{path}/{file_name}.svg",
+                parent_width=800,
+                parent_height=600,
+                output_width=1000,
+                write_to=f"{path}/{file_name}.png",
+            )
     # im = Image.open(f"{destination_path}/{file_name}.png").convert("RGB")
+    # if os.path.isfile(f"{path}/{file_name}.png") and not os.path.isfile(f"{path}/{file_name}.webp"):
+
+    if ext == "jpg" or ext == "jpeg":
+        im = Image.open(f"{path}/{file_name}.{ext}").convert("RGB")
+        im.save(
+            f"{path}/{file_name}.png",
+            format="png",
+            lossless=True,
+            quality=90,
+            method=6,
+            minimize_size=True,
+            allow_mixed=True,
+        )
+
     im = Image.open(f"{path}/{file_name}.png")
     im.save(
         f"{path}/{file_name}.webp",
