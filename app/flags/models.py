38cf0ee955ef49e97bc0300602fdd340445eb45d
Alex Baikov <baikov52@gmail.com>
2021-08-09 12:48:54 +0300

    temporary removed png_file field from HistoricalFlag model

--- app/flags/models.py
+++ app/flags/models.py
@@ -101,9 +101,6 @@ class Region(Seo, models.Model):
         else:
             return reverse("flags:region-flags", args=[self.slug])
 
-    # def get_absolute_url(self):
-    #     return reverse("flags:region-detail", kwargs={"slug": self.slug})
-
 
 class Country(Seo, models.Model):
     name = models.CharField(verbose_name=_("Name"), max_length=250)
@@ -308,6 +305,7 @@ class HistoricalFlag(models.Model):
     to_year = models.PositiveSmallIntegerField(verbose_name=_("Ended year"))
     image_url = models.URLField(verbose_name=_("SVG image link"), max_length=300, blank=True)
     svg_file = models.FileField(verbose_name=_("SVG image"), upload_to="historical-flags/", blank=True)
+    # png_file = models.FileField(verbose_name=_("PNG image"), upload_to="historical-flags/", blank=True)
     description = models.TextField(verbose_name=_("Hstorical flag description"), blank=True)
 
     class Meta:
