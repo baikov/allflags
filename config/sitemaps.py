9b97c85dd28ed377fe31e566788d5c4334340a02
Alex Baikov <baikov52@gmail.com>
2021-08-09 11:52:05 +0300

    added sitemaps.py

---
+++ config/sitemaps.py
@@ -0,0 +1,62 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls.base import reverse
+
+from app.flags.models import ColorGroup, FlagElement, MainFlag, Region
+
+
+class MainFlagSitemap(Sitemap):
+    protocol = 'https'
+
+    def items(self):
+        return MainFlag.objects.filter(is_index__exact=True, is_published__exact=True)
+
+    def lastmod(self, obj):
+        return obj.updated_date
+
+
+class RegionSitemap(Sitemap):
+    protocol = 'https'
+
+    def items(self):
+        return Region.objects.filter(is_index__exact=True, is_published__exact=True)
+
+    def lastmod(self, obj):
+        return obj.updated_date
+
+
+class FlagElementSitemap(Sitemap):
+    protocol = 'https'
+
+    def items(self):
+        return FlagElement.objects.filter(is_index__exact=True, is_published__exact=True)
+
+    def lastmod(self, obj):
+        return obj.updated_date
+
+    def location(self, obj) -> str:
+        return reverse('flags:element-detail', args=[obj.slug])
+
+
+class ColorGroupSitemap(Sitemap):
+    protocol = 'https'
+
+    def items(self):
+        return ColorGroup.objects.filter(is_index__exact=True, is_published__exact=True)
+        # return ColorGroup.objects.all().distinct('color_group')
+
+    def lastmod(self, obj):
+        return obj.updated_date
+
+    def location(self, obj) -> str:
+        return reverse('flags:color-detail', args=[obj.slug])
+        # return reverse('flags:colors-count', args=[3])
+
+
+class StaticViewSitemap(Sitemap):
+    protocol = 'https'
+
+    def items(self):
+        return ['about', 'flags:regions-list', 'flags:colors-list', 'flags:elements-list']
+
+    def location(self, item):
+        return reverse(item)
