01085257faaa523ca6e34a2f0687d13aaeb342da
Alex Baikov <baikov52@gmail.com>
2021-08-09 11:51:42 +0300

    added sitemaps

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,27 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps import views
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
+
+from config.sitemaps import (
+    ColorGroupSitemap,
+    FlagElementSitemap,
+    MainFlagSitemap,
+    RegionSitemap,
+    StaticViewSitemap,
+)
+
+sitemaps = {
+    'flags': MainFlagSitemap,
+    'static': StaticViewSitemap,
+    'colors': ColorGroupSitemap,
+    'regions': RegionSitemap,
+    'elements': FlagElementSitemap,
+}
 
 urlpatterns = [
     # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -17,8 +34,8 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("", include("app.flags.urls", namespace="flags")),
-    # path('sitemap.xml', views.index, {'sitemaps': sitemaps}),
-    # path("sitemap-<section>.xml", views.sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path('sitemap.xml', views.index, {'sitemaps': sitemaps}),
+    path("sitemap-<section>.xml", views.sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
