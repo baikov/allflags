52bcfd5d21185dfc5fa595080212be66031836a9
Alexey Baikov <baikov52@gmail.com>
2021-09-02 21:58:06 +0300

    added download image func

--- app/flags/services.py
+++ app/flags/services.py
@@ -1,6 +1,58 @@
+import io
 import os
+import re
+import urllib
+
+import cairosvg
+from defusedxml.common import EntitiesForbidden
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
 
 from config.settings.base import MEDIA_ROOT
+
+
+def download_image(url: str) -> SimpleUploadedFile:
+    path, file_name = os.path.split(url)
+    name, ext = os.path.splitext(file_name)
+    ext = ext.lower()
+
+    file = io.BytesIO(urllib.request.urlopen(url).read())
+
+    if ext == ".svg":
+        new = io.BytesIO()
+        try:
+            cairosvg.svg2png(file_obj=file, output_width=800, write_to=new)
+        except EntitiesForbidden:
+            rgx_list = [r"<!ENTITY .*?>", r"xmlns=.*?;\"", r"xmlns:xlink=.*?;\""]
+            file.seek(0)
+            file_str = file.read().decode("UTF-8")
+            for rgx_match in rgx_list:
+                file_str = re.sub(rgx_match, "", file_str)
+            file = io.BytesIO(file_str.encode("UTF-8"))
+            cairosvg.svg2png(file_obj=file, output_width=800, write_to=new)
+        except Exception as e:
+            return e
+        new.seek(0)
+        file = new
+        ext = ".png"
+
+    img = Image.open(file)
+    format = img.format
+    width, height = img.size
+    width, height = scale_dimensions(width, height, longest_side=800)
+    img = img.resize((width, height), Image.ANTIALIAS)
+    img.save(file, format=format)
+    file.seek(0)
+    return SimpleUploadedFile(f"{name}{ext}", file.read())
+
+
+def scale_dimensions(width: int, height: int, longest_side: int) -> tuple:
+    ratio = width / height
+    if ratio > 1:
+        return longest_side, int(longest_side / ratio)
+    # Portrait
+    else:
+        return int(longest_side * ratio), longest_side
 
 
 def img_path_by_flag_type(instance, filename) -> str:
