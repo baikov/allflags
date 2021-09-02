c30421de0c0424e1c99b691c9569a97208d22bfb
Alexey Baikov <baikov52@gmail.com>
2021-09-02 22:57:57 +0300

    some fixes

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps import views
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
