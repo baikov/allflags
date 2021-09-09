391bf9dc95d9fe03d55356b1eb61505e043df971
Alexey Baikov <baikov52@gmail.com>
2021-09-09 17:19:21 +0300

    reduced the number of requests

--- app/flags/context_processors.py
+++ app/flags/context_processors.py
@@ -1,10 +1,23 @@
+from django.db.models import Count
+
 from app.flags.models import Region
 
 
 def regions_menu(request):
     if request.user.is_superuser:
-        regions = Region.objects.filter(parent=None).order_by("ordering")
+        regions = Region.objects.prefetch_related("subregions").filter(parent=None).order_by("ordering")
+        subregions = Region.objects.select_related("parent").annotate(countries_count=Count("countries"))
     else:
-        regions = Region.objects.filter(parent=None, is_published=True).order_by("ordering")
+        # regions = (
+        #     Region.objects.prefetch_related("subregions").filter(parent=None, is_published=True).order_by("ordering")
+        # )
+        regions = (
+            Region.objects.prefetch_related("subregions").filter(parent=None, is_published=True).order_by("ordering")
+        )
+        subregions = (
+            Region.objects.select_related("parent")
+            .filter(is_published=True)
+            .annotate(countries_count=Count("countries"))
+        )
 
-    return {"regions_menu": regions}
+    return {"regions_menu": regions, "subregions": subregions}
