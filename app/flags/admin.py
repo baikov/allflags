c00b9de138f1792e73c141ba24b01a04c020ae47
Alexey Baikov <baikov52@gmail.com>
2021-10-04 11:24:16 +0300

    some fixies for FlagFactAdmin

--- app/flags/admin.py
+++ app/flags/admin.py
@@ -451,13 +451,13 @@ class HistoricalFlagAdmin(admin.ModelAdmin):
 
 
 class FlagFactAdmin(admin.ModelAdmin):
-    list_display = ("flag", "caption", "label", "ordering")
+    list_display = ("flag", "caption", "label", "ordering", "is_published")
     search_fields = ["flag__country__name", "caption", "label"]
     # list_filter = ["flag__country__region", "flag__country"]
     raw_id_fields = ("flag",)
-    list_editable = ("ordering",)
+    list_editable = ("ordering", "is_published")
     fieldsets = [
-        (None, {"fields": ["flag", "caption", ("ordering", "label"), "text", "image"]}),
+        (None, {"fields": ["flag", "caption", ("ordering", "is_published", "label"), "text", "image"]}),
     ]
 
 
