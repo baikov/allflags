a7190ef2715db61e83863109c4f13774bd6b36bc
Alexey Baikov <baikov52@gmail.com>
2021-10-04 11:30:09 +0300

    download imgs from cdn with celery

--- app/flags/tasks.py
+++ app/flags/tasks.py
@@ -1,8 +1,14 @@
 from celery import shared_task
 
-from app.utils.flag_image import get_flag_img
+# from app.utils.flag_image import get_flag_img
+from app.flags.services import get_img_from_cdn
+
+
+# @shared_task(bind=True, soft_time_limit=100, time_limit=120)
+# def get_flag_img_task(self, iso2):
+#     get_flag_img(iso2)
 
 
 @shared_task(bind=True, soft_time_limit=100, time_limit=120)
-def get_flag_img_task(self, iso2):
-    get_flag_img(iso2)
+def get_img_from_cdn_task(self, flag_id, iso2):
+    get_img_from_cdn(flag_id, iso2)
